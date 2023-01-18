b88fab51ab5efff8d0ebf3c528a656f06ddb8d7b
frederico <fsbettencourt22@gmail.com>
2023-01-18 16:17:09 +0000

    nav bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
+import SimpleBottomNavigation from "./components/Nav";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
-    </div>
+      <div className="app"></div>
+      <SimpleBottomNavigation />
+    </>
   );
 }
 
