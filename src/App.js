36b8e392b25fecea41f1cdc602864a3dd91c719f
frederico <fsbettencourt22@gmail.com>
2023-01-28 00:06:57 +0000

    css cards and btn

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Header } from "./components/Header/Header";
 import { Watchlist } from "./components/Watchlist/Watchlist";
 import { Watched } from "./components/Watched/Watched";
 import { Add } from "./components/Add/Add";
-
 import "./App.css";
 
 function App() {
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -2,10 +2,14 @@ import React from "react";
 import "./Cards.css";
 
 export const Cards = ({ movie }) => {
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
   return (
     <div className="result-card">
       <div className="poster-wrapper">
-        {movie.poster__path ? (
+        {movie.poster_path ? (
           <img
             src={`https:/image.tmdb.org/t/p/w200${movie.poster_path}`}
             alt={`${movie.title}Poster`}
@@ -17,7 +21,14 @@ export const Cards = ({ movie }) => {
       <div className="info">
         <div className="film_info">
           <h3 className="title">{movie.title}</h3>
-          <h4 className="release-date">{movie.release_date.substring(0, 4)}</h4>
+          <h4 className="discription">{truncate(movie?.overview, 150)}</h4>
+          <h4 className="release-date">
+            {movie.release_date ? (
+              movie.release_date.substring(0, 4)
+            ) : (
+              <div className="filler-text"></div>
+            )}
+          </h4>
         </div>
         <div className="controls">
           <button className="btn">Add to Watchlist</button>
